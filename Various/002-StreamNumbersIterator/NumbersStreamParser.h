7082b62557f8bda8b60fd14559803c0c1ba25cba
Liviu Andron <liviu.andron.work@gmail.com>
2016-01-25 00:54:50 +0100

    stream numbers iterator test

---
+++ Various/002-StreamNumbersIterator/NumbersStreamParser.h
@@ -0,0 +1,162 @@
+
+/*
+    //  Imposed interface
+
+    class Solution {
+    public:
+        Solution(istream& s): m_src(s) {}
+        class iterator;
+
+        iterator begin();
+        iterator end();
+    }
+
+    Allowed numbers, only one per line: spaces, +/-, any number of zeros, number, spaces
+        |number| < 1000000000
+*/
+
+#include <iosfwd>
+#include <string>
+#include <sstream>
+#include <cassert>
+using namespace std;
+
+class Solution {
+    std::istream& m_src;
+public:
+    Solution(istream& s): m_src(s) {}
+    class iterator;
+
+    iterator begin();
+    iterator end();
+
+    class iterator
+    {
+        istream& m_src;
+        streampos m_pos;
+        bool m_finished;
+        int m_n;
+
+        void readNextLine(std::string&);
+        bool getInteger(const std::string&, int&);
+
+    public:
+        iterator(istream& s, streampos pos, bool finished):m_src(s), m_pos(pos), m_n(0), m_finished(finished) {}
+
+        bool operator==(const iterator& rhs) const {return (m_finished == rhs.m_finished) && (m_finished || (m_pos == rhs.m_pos));}
+        //  TODO;   is this necessary ?
+        bool operator!=(const iterator& rhs) const {return !operator==(rhs);}
+        iterator& operator++();
+        iterator operator++(int) {iterator tmp(*this); operator++(); return tmp;}
+
+        //  OPT:    should throw for end() ?
+        int operator*() const {return m_n;}
+    };
+};
+
+void Solution::iterator::readNextLine(std::string& line)
+{
+    assert(!m_src.eof());
+
+    stringbuf sb;
+    m_src.get(sb, 10);
+    line = sb.str();
+
+    //  skip new line
+    if (!m_src.eof())
+        m_src.seekg(1, std::ios_base::cur);
+    m_pos = m_src.tellg();
+    //  m_finished not here since we want one last read
+}
+
+bool Solution::iterator::getInteger(const std::string& line, int& n)
+{
+    size_t i;
+    //  trim
+    for (i = 0; (i < line.length()) && (' ' == line[i] || '\t' == line[i]); ++i);
+    if (i == line.length())
+        return false;
+    // sign
+    const bool negative = (line[i] == '-');
+    if (negative || (line[i] == '+'))
+        ++i;
+    // eat zeros
+    for (; (i < line.length()) && ('0' == line[i]); ++i);
+    if (i == line.length())
+        return (i == 0) ? false : ('0' == line[i-1]);
+    //  number
+    size_t j;
+    for (j = i; (j < line.length()) && ('0' <= line[j]) && ('9' >= line[j]); ++j);
+    //  trim
+    size_t k;
+    for (k = j; (k < line.length()) && (' ' == line[k] || '\t' == line[k]); ++k);
+    if (k != line.length())
+        return false;
+
+    //  too big
+    if (j - i > 10)
+        return false;
+    if (j - i == 10)
+    {
+        if (line[i] != '1')
+            return false;
+        for (k = i+1; k < j; ++k)
+            if (line[k] != '0')
+                return false;
+    }
+    
+    n = 0;
+    for (k = i; k < j; ++k)
+        n = n*10 + (line[k] - '0');
+    if (negative)
+        n = -n;
+
+    return true;
+}
+
+Solution::iterator Solution::begin()
+{
+    streampos restore = m_src.tellg();
+    m_src.seekg(0, ios_base::beg);
+    Solution::iterator begin(m_src, m_src.tellg(), false);
+    ++begin;
+    m_src.seekg(restore);
+    return begin;
+}
+
+Solution::iterator Solution::end()
+{
+    return Solution::iterator(m_src, 0 /*it doesn't matter*/, true);
+}
+
+Solution::iterator& Solution::iterator::operator++()
+{
+    assert(!m_src.eof());
+
+    //  necessary: we are using one stream only
+    m_src.seekg(m_pos);
+
+    for (m_finished = m_src.eof(); !m_finished; m_finished = m_src.eof())
+    {
+        std::string line;
+        readNextLine(line);
+        int n;
+        if (getInteger(line, n))
+        {
+            m_n = n;
+            break;
+        } 
+    }
+
+    return *this;
+}
+
+/**
+ * Example usage:
+ *
+ *   Solution sol(stream);
+ *   for (Solution::iterator it = sol.begin(); it != sol.end(); ++it) {
+ *      int x = *it;
+ *      cout << x << endl;
+ *   }
+ */
---
+++ Various/002-StreamNumbersIterator/NumbersStreamParser_Test.cpp
@@ -0,0 +1,130 @@
+
+#include <cassert>
+#include <sstream>
+#include <iostream> //cout
+
+#include "NumbersStreamParser.h"
+
+void FileNumbersParser_Test_Invalids()
+{
+    static const char* test[] =
+    {
+        "\
+            123\
+            -54\
+        "
+        "",
+        "fides",
+        "--3",
+        "-1000000001",
+        "+1000000001",
+        "3 45",
+    };
+    for (size_t i = 0; i < sizeof(test)/sizeof(test[0]); ++i)
+    {
+        std::stringbuf buf(test[i]);
+        std::istream ss(&buf);
+
+        Solution sol(ss);
+        assert(sol.begin() == sol.end());
+    }
+}
+
+void FileNumbersParser_Test_Valids_1line()
+{
+    static const char* test[] =
+    {
+        "\
+            123\n\
+            -5y4\n\
+        ",
+        "-3",
+        "3",
+        "+3",
+        "+0000000000003",
+        "0000000000003",
+        " 03   \t",
+        "-030",
+        "+0999999999",
+        "-0999999999",
+        "-1000000000",
+        "+1000000000"
+    };
+    for (size_t i = 0; i < sizeof(test)/sizeof(test[0]); ++i)
+    {
+        std::stringbuf buf(test[i]);
+        std::istream ss(&buf);
+
+        Solution sol(ss);
+        Solution::iterator it = sol.begin();
+        assert(it != sol.end());
+        ++it;
+        assert(it == sol.end());
+    }
+}
+
+void FileNumbersParser_Test_Valids_2lines()
+{
+    static const char* test[] =
+    {
+        "\
+            123\n\
+            -54\n\
+        ",
+        "\
+            123\n\
+            -5.4\n\
+            0.1\n\
+            00000000000000000\n\
+        ",
+        "\
+            -5\n\
+            +0000000000\n\
+        ",
+    };
+    for (size_t i = 0; i < sizeof(test)/sizeof(test[0]); ++i)
+    {
+        std::stringbuf buf(test[i]);
+        std::istream ss(&buf);
+
+        Solution sol(ss);
+        Solution::iterator it = sol.begin();
+        assert(it != sol.end());
+        ++it;
+        assert(it != sol.end());
+        ++it;
+        assert(it == sol.end());
+    }
+}
+
+void FileNumbersParser_Test_1()
+{
+    static const char* test = 
+    "\
+        123\n\
+        -54\n\
+        haj\n\
+        +000000000001\n\
+        -4540\n\
+        1119\n\
+        3232\n\
+        +0000004321     \n\
+        +2000000000\n\
+    ";
+    std::stringbuf test2(test);
+    std::istream ss(&test2);
+
+    Solution sol(ss);
+    for (Solution::iterator it = sol.begin(); it != sol.end(); ++it) {
+        int x = *it;
+        std::cout << x << endl;
+    }
+}
+
+void FileNumbersParser_Tests()
+{
+    FileNumbersParser_Test_Invalids();
+    FileNumbersParser_Test_Valids_1line();
+    FileNumbersParser_Test_Valids_2lines();
+    FileNumbersParser_Test_1();
+}
