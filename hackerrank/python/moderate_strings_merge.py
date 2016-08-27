42b87d4d2da075772dab8eb49b65a7c2013b830b
Liviu Andron <liviu.andron.work@gmail.com>
2016-08-27 21:32:40 +0200

    hackerrank, python, moderate

---
+++ hackerrank/python/moderate_strings_merge.py
@@ -0,0 +1,57 @@
+'''
+    https://www.hackerrank.com/challenges/merge-the-tools
+
+    Version 2016.08.27
+
+    >pylint --version
+        No config file found, using default configuration
+        pylint 1.5.5,
+        astroid 1.4.5
+        Python 3.5.1 (v3.5.1:37a07cee5969, Dec  6 2015, 01:38:48) [MSC v.1900 32 bit (Intel)]
+    Your code has been rated at 10.00/10
+'''
+
+import io
+
+def solve_with_iofunc(func):
+    '''solve the problem getting text using the given function'''
+    s_input = func().strip()
+    segments_no = int(func().strip())
+    result = ''
+    for i in range(len(s_input)//segments_no):
+        if i:
+            result += '\n'
+        alf = [False] * 32
+        for j in s_input[i*segments_no : (i+1)*segments_no]:
+            if not alf[ord(j)-65]:
+                alf[ord(j)-65] = True
+                result += j
+    return result
+
+def solve_with_text(text):
+    '''hackerrank test function'''
+    buf = io.StringIO(text)
+    return solve_with_iofunc(buf.readline)
+
+def solve_with_file(filename):
+    '''hackerrank test function: get input from frile'''
+    file = open(filename, 'r')
+    return solve_with_iofunc(file.readline)
+
+def read_and_solve():
+    '''Hackerrank test function'''
+    print(solve_with_iofunc(input))
+
+def debug_validations():
+    '''unit testing'''
+    assert solve_with_text('AABCAAADA\n3 ') == 'AB\nCA\nAD'
+    assert solve_with_text('AABCAAADAZ\n2') == 'A\nBC\nA\nAD\nAZ'
+    assert solve_with_text('AABCAAADAZ\n5') == 'ABC\nADZ'
+
+def main():
+    '''main function: accessible from exterior'''
+    debug_validations()
+    # read_and_solve()
+
+if __name__ == "__main__":
+    main()
