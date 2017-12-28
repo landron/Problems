d97e77f3b07f68e59528b5199def326ec0b9ef34
Liviu Andron <liviu.andron.work@gmail.com>
2017-12-28 03:44:59 +0100

    magic squares permutations

--- Canon/permutations.py
+++ Canon/permutations.py
@@ -15,6 +15,7 @@ pylint --version
 
 import math
 
+#   tag_next
 #
 #   permutations without full recursivity
 #     - permutations_get_prev is still recursive
@@ -113,7 +114,7 @@ def gen_permutations_2_rec(limit, seq_len, partial, print_it):
             continue
         partial.seq[seq_len] = next_val
 
-        # print('gen_permutations_2_rec_111', seq_len, seq, next_val, taken)
+        # print('gen_permutations_2_rec_111', seq_len, partial.seq, next_val, partial.taken)
 
         if seq_len+1 == limit:
             if print_it:
@@ -155,6 +156,7 @@ def gen_permutations(limit):
     '''
     return gen_permutations_2(limit)
 
+#   tag_next
 #
 #   combinations without full recursivity
 #
@@ -231,6 +233,133 @@ def gen_combinations(limit, size):
     '''
     return gen_combinations_1(limit, size)
 
+#   tag_next
+#
+#   generate magic squares using permutations:
+#       they contain distinct digits and
+#       the sums of the lines, columns and diagonals are the same
+#
+
+def sum_v(seq, where, limit_side, needed_sum):
+    '''calcutae the sum of a column'''
+    sump = 0
+    for i in range(where, -1, -limit_side):
+        sump += (1+seq[i])
+        if sump > needed_sum:
+            return 0
+    return sump
+
+def validate_v(seq_len, seq, limit_side, is_final):
+    '''see that the columns of the square have the correct sum'''
+    needed_sum = limit_side*(limit_side*limit_side+1)//2
+    beginning = seq_len//limit_side*limit_side
+    for i in range(0, limit_side):
+        sump = sum_v(seq, beginning+i, limit_side, needed_sum)
+        if sump == 0:
+            return False
+        # print(beginning+i, sump)
+        if is_final and sump != needed_sum:
+            return False
+    return True
+
+def validate_diag(seq, limit_side):
+    '''see that the diagonal of the square has the correct sum'''
+    diag_1 = diag_2 = 0
+    for i in range(0, limit_side):
+        diag_1 += (1+seq[i*limit_side+i])
+        diag_2 += (1+seq[i*limit_side+limit_side-i-1])
+    needed_sum = limit_side*(limit_side*limit_side+1)//2
+    # print(diag_1, diag_2)
+    return diag_1 == needed_sum and diag_2 == needed_sum
+
+def print_fancy(seq):
+    '''print square starting from value 1 (instead of 0)'''
+    fancy = [0]*len(seq)
+    for i, elem in enumerate(seq):
+        fancy[i] = elem+1
+    print(fancy)
+
+def gen_square_permutations_rec(limit_side, seq_len, partial, print_it):
+    '''
+        Purpose:    the recursive worker of this permutations variant
+
+        The third parameter is a structure to avoid 'too-many-arguments'
+    '''
+    assert seq_len < limit_side*limit_side
+
+    limit = limit_side*limit_side
+
+    # sum (1 .. limit_side*limit_side)/limit_side
+    assert (limit_side*(limit+1))%2 == 0
+    needed_sum = limit_side*(limit+1)//2
+
+    taken = partial.taken
+    seq = partial.seq
+
+    sump = 0
+    for i in range(seq_len//limit_side*limit_side, seq_len):
+        sump += (1+seq[i])
+
+    for next_val in range(limit):
+        if taken[next_val]:
+            continue
+
+        if sump + (1+next_val) >= needed_sum:
+            continue
+
+        seq[seq_len] = next_val
+
+        if (seq_len+2)%limit_side == 0:
+            needed_val = needed_sum - sump - (1+next_val) - 1
+            if needed_val >= limit or taken[needed_val] or needed_val == next_val:
+                continue
+
+            seq[seq_len+1] = needed_val
+
+            # the validations functions (validate_v, validate_diag) could be tested with each step
+            if seq_len+2 == limit:
+                # partial.evaluated += 1 
+                if not validate_v(seq_len, seq, limit_side, True):
+                    continue
+                if not validate_diag(seq, limit_side):
+                    continue
+                if print_it:
+                    # print("Solution", seq)
+                    print_fancy(seq)
+                partial.found += 1
+                continue
+
+            taken[next_val] = True
+            taken[needed_val] = True
+
+            gen_square_permutations_rec(limit_side, seq_len+2, partial, print_it)
+
+            taken[needed_val] = False
+            taken[next_val] = False
+
+        else:
+            taken[next_val] = True
+            gen_square_permutations_rec(limit_side, seq_len+1, partial, print_it)
+            taken[next_val] = False
+
+def gen_square_permutations(limit, print_it=False):
+    '''
+        Purpose:    permutations forming a limit*limit square such as the
+                    following sums are equals: rows, columns, diagonals
+    '''
+    assert limit != 0
+
+    partial = lambda: None
+    partial.taken = [False] * (limit*limit)
+    partial.seq = [0] * (limit*limit)
+    partial.found = 0
+    # partial.evaluated = 0
+
+    gen_square_permutations_rec(limit, 0, partial, print_it)
+    if print_it:
+        print("Found({0}):".format(limit), partial.found)
+    return partial.found
+
 #
 #   main
 #
@@ -247,14 +376,27 @@ def debug_assertions():
         for j in range(2, i):
             assert combinations_no(i, j) == gen_combinations_1(i, j)
 
+    assert validate_v(8, [3, 8, 1, 2, 4, 6, 7, 0, 5], 3, True)
+    assert validate_diag([3, 8, 1, 2, 4, 6, 7, 0, 5], 3)
+    assert validate_v(15, [0, 1, 15, 14, 12, 13, 3, 2, 11, 6, 8, 5, 7, 10, 4, 9], 4, True)
+    assert validate_diag([0, 1, 15, 14, 12, 13, 3, 2, 11, 6, 8, 5, 7, 10, 4, 9], 4)
+    assert validate_v(15, [0, 2, 13, 15, 14, 12, 3, 1, 9, 5, 10, 6, 7, 11, 4, 8], 4, True)
+    assert validate_diag([0, 2, 13, 15, 14, 12, 3, 1, 9, 5, 10, 6, 7, 11, 4, 8], 4)
+    assert validate_v(15, [0, 3, 12, 15, 11, 14, 1, 4, 13, 8, 7, 2, 6, 5, 10, 9], 4, True)
+    assert validate_diag([0, 3, 12, 15, 11, 14, 1, 4, 13, 8, 7, 2, 6, 5, 10, 9], 4)
+    assert gen_square_permutations(2) == 0
+    assert gen_square_permutations(3) == 8
+
 def main():
     '''main function'''
     debug_assertions()
 
     # tests
-    # print(gen_permutations_1(7, True))
+    # print(gen_permutations_1(7, True), math.factorial(7))
     # print(gen_combinations_1(10, 5, True), combinations_no(10, 5))
     # print(gen_permutations_2(5, True), math.factorial(5))
+    assert math.factorial(16) == 20922789888000, "gen_square_permutations(4 is too much"
+    print(gen_square_permutations(3, True), math.factorial(9))
 
 if __name__ == "__main__":
     main()
