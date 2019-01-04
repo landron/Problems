b958190297baa3507c2f7b160ff8ba63f166a870
Liviu <liviu@meetlima.com>
2019-01-04 13:59:37 +0100

    connected components (graph theory):  DFS - BFS confusion (DFS is harder than it seemed)

--- hackerrank/algo/Graph Theory/easy_connected_components.py
+++ hackerrank/algo/Graph Theory/easy_connected_components.py
@@ -6,15 +6,74 @@
     tag_graph, tag_dfs (Depth-first search)
 
     breadth-first search is an alternative, depending on how we extract
-        see pop below
+        see dfs vs bfs
 '''
-# import sys
 
 
 def dfs(grid, components, row, column):
     '''
         Depth-first search (DFS) is an algorithm for traversing
         or searching tree or graph data structures.
+
+        go as down as possible in the list, then start processing
+            nodes
+    '''
+    def good_coord(cols, rows, coord_x, coord_y):
+        # has curr valid coordinates ?
+        if coord_x < 0 or coord_y < 0:
+            return False
+        if rows <= coord_x or cols <= coord_y:
+            return False
+        return True
+
+    def next_child(grid, orig_x, orig_y):
+        rows_no = len(grid)
+        cols_no = len(grid[0])
+
+        for x_rel in [-1, 0, 1]:
+            curr_x = orig_x + x_rel
+            for y_rel in [-1, 0, 1]:
+                curr_y = orig_y + y_rel
+                if not good_coord(cols_no, rows_no, curr_x, curr_y):
+                    continue
+                if components[curr_x][curr_y].seen:
+                    continue
+                # only 1s are actives : not needed (from initialization)
+                assert grid[curr_x][curr_y]
+                if not grid[curr_x][curr_y]:
+                    continue
+
+                return curr_x, curr_y
+
+        return None
+
+    component_id = components[row][column].id
+    components[row][column].seen = True
+
+    to_process = [(row, column)]
+
+    while to_process:
+        orig_x, orig_y = to_process[0]
+
+        child = next_child(grid, orig_x, orig_y)
+        if child:
+            curr_x, curr_y = child
+            components[curr_x][curr_y].id = component_id
+            components[curr_x][curr_y].seen = True
+
+            to_process.append((curr_x, curr_y))
+        else:
+            # process ... : do nothing! (no-op)
+            to_process.pop(0)
+
+
+def bfs(grid, components, row, column):
+    '''
+        Breadth-first search (BFS) is an algorithm for traversing
+        or searching tree or graph data structures.
+
+        just use a queue and expand it with all the "unseen" (not processed)
+            children
     '''
     def good_coord(cols, rows, coord_x, coord_y):
         # has curr valid coordinates ?
@@ -33,8 +92,7 @@ def dfs(grid, components, row, column):
     to_process = [(row, column)]
 
     while to_process:
-        # the diffence between DFS and BFS
-        orig_x, orig_y = to_process.pop(0)
+        orig_x, orig_y = to_process.pop()
 
         for x_rel in [-1, 0, 1]:
             curr_x = orig_x + x_rel
@@ -88,7 +146,10 @@ def solve_components(grid):
             if not grid[i][j] or next_el.seen:
                 next_el.seen = True
                 continue
+
             dfs(grid, components, i, j)
+            # or BFS
+            # bfs(grid, components, i, j)
 
     if 0:  # pylint: disable=using-constant-test
         for i, line in enumerate(components):
